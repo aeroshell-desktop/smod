f149d75a1436f081a1c1e5c91acea0c01bbc48c3
catpswin56 <catpswin5@proton.me>
2026-03-21 23:56:56 -0300

    Installation scripts: rely on CMAKE_GENERATOR instead of having a --ninja argument

--- install.sh
+++ install.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
 BUILD_DST="build"
-USE_NINJA=
-BUILD_COMMAND="make"
 
 SU_CMD=sudo
-
-if [[ "$*" == *"--ninja"* ]]
-then
-    if [[ -z "$(command -v ninja)" ]]; then
-        echo "Attempted to build using Ninja, but Ninja was not found on the system. Falling back to GNU Make."
-    else
-        echo "Compiling using Ninja"
-        USE_NINJA="-G Ninja"
-        BUILD_COMMAND="ninja"
-    fi
-fi
 
 if [[ -z "$(command -v $SU_CMD)" ]]; then
     SU_CMD=doas
@@ -27,7 +14,7 @@ fi
 
 rm -rf "$BUILD_DST"
 mkdir -p "$BUILD_DST"
-cmake -DCMAKE_INSTALL_PREFIX=/usr -B build $USE_NINJA .
+cmake -DCMAKE_INSTALL_PREFIX=/usr -B "$BUILD_DST" .
 cmake --build "$BUILD_DST"
 $SU_CMD cmake --install "$BUILD_DST"
 
--- smodglow/install.sh
+++ smodglow/install.sh
@@ -3,21 +3,8 @@
 BUILD_DST="build"
 BUILD_PLATFORM="X11"
 BUILD_PARAM=
-USE_NINJA=
-BUILD_COMMAND="make"
 
 SU_CMD=sudo
-
-if [[ "$*" == *"--ninja"* ]]
-then
-    if [[ -z "$(command -v ninja)" ]]; then
-        echo "Attempted to build using Ninja, but Ninja was not found on the system. Falling back to GNU Make."
-    else
-        echo "Compiling using Ninja"
-        USE_NINJA="-G Ninja"
-        BUILD_COMMAND="ninja"
-    fi
-fi
 
 if [[ -z "$(command -v $SU_CMD)" ]]; then
     SU_CMD=doas
@@ -34,13 +21,10 @@ then
     BUILD_PLATFORM="Wayland"
     BUILD_PARAM="-DKWIN_BUILD_WAYLAND=ON"
 fi
-rm -rf "${BUILD_DST}"
-mkdir "${BUILD_DST}"
-cd "${BUILD_DST}"
-
 
 echo "Building ${BUILD_PLATFORM} effect..."
-cmake ../ -DCMAKE_INSTALL_PREFIX=/usr $BUILD_PARAM $USE_NINJA
 
-$BUILD_COMMAND
-$SU_CMD $BUILD_COMMAND install
+cmake -DCMAKE_INSTALL_PREFIX=/usr $BUILD_PARAM -B "$BUILD_DST" .
+cmake --build "$BUILD_DST"
+$SU_CMD cmake --install "$BUILD_DST"
+
